b3ef7bf29c13113b5005ebcd63c3cd3e2a01a20a
Marlon Colca <marlon.colca.io@gmail.com>
2019-06-28 11:41:53 -0500

    Ajustes al estilo de codigo de acuerdo a houndiy

--- app/controllers/PrimaryController.rb
+++ app/controllers/PrimaryController.rb
@@ -16,7 +16,7 @@ class PrimaryController
     email = @client.escape(value.email)
     password = @client.escape(value.password)
 
-    query = "INSERT INTO Alumno (nombre, apellido, email, password) VALUES ("
+    query = 'INSERT INTO Alumno (nombre, apellido, email, password) VALUES ('
     query += "'#{nombre}', '#{apellido}', '#{email}', '#{password}');" 
 
     results = @client.query(query)
@@ -29,7 +29,7 @@ class PrimaryController
     email = @client.escape(value.email)
     password = @client.escape(value.password)
 
-    query = "INSERT INTO Docente (nombre, apellido, email, password) VALUES ("
+    query = 'INSERT INTO Docente (nombre, apellido, email, password) VALUES ('
     query += "'#{nombre}', '#{apellido}', '#{email}', '#{password}');" 
 
     results = @client.query(query)
@@ -37,13 +37,13 @@ class PrimaryController
   end
 
   def insert_taller (value)
-    validateInteger!("codigo_docente", value.codigo_docente)
+    validateInteger!('codigo_docente', value.codigo_docente)
 
     nombre = @client.escape(value.nombre)
     tipoTaller = @client.escape(value.tipoTaller)
     codigo_docente = value.codigo_docente
 
-    query = "INSERT INTO Taller (nombre, tipoTaller, codigo_docente) VALUES ("
+    query = 'INSERT INTO Taller (nombre, tipoTaller, codigo_docente) VALUES ('
     query += "'#{nombre}', '#{tipoTaller}', '#{codigo_docente}');" 
 
     results = @client.query(query)
@@ -51,13 +51,13 @@ class PrimaryController
   end
 
   def insert_matricula (value)
-    validateInteger!("codigo_alumno", value.codigo_alumno)
-    validateInteger!("codigo_taller", value.codigo_taller)
+    validateInteger!('codigo_alumno', value.codigo_alumno)
+    validateInteger!('codigo_taller', value.codigo_taller)
 
     codigo_alumno = value.codigo_alumno
     codigo_taller = value.codigo_taller
 
-    query = "INSERT INTO Matricula (codigo_alumno, codigo_taller) VALUES ("
+    query = 'INSERT INTO Matricula (codigo_alumno, codigo_taller) VALUES ('
     query += "#{codigo_alumno}, #{codigo_taller});" 
 
     results = @client.query(query)
@@ -65,31 +65,30 @@ class PrimaryController
   end
 
   def get_alumno (id)
-    validateInteger!("id", d)
+    validateInteger!('id', d)
     query = "SELECT codigo, nombre, apellido FROM Alumno WHERE codigo = #{id}"
     return queryToArray(query)
   end
 
   def get_docente (id)
-    validateInteger!("id", id)
+    validateInteger!('id', id)
     query = "SELECT codigo, nombre, apellido FROM Docente WHERE codigo = #{id}"
     return queryToArray(query)
   end
 
   def get_taller (id)
-    validateInteger!("id", id)
+    validateInteger!('id', id)
     query = "SELECT codigo, nombre, tipoTaller, codigo_docente FROM Taller WHERE codigo = #{id}"
     return queryToArray(query)
   end
 
   def get_matricula (codigo_alumno, codigo_taller)
-    validateInteger!("codigo_alumno", codigo_alumno)
-    validateInteger!("codigo_taller", codigo_taller)
+    validateInteger!('codigo_alumno', codigo_alumno)
+    validateInteger!('codigo_taller', codigo_taller)
 
     query = "SELECT codigo_alumno, codigo_taller, eval1, eval2, evalFinal FROM Matricula "
     query += "WHERE codigo_alumno = #{codigo_alumno} AND codigo_taller = #{codigo_taller}"
     return queryToArray(query)
-  
   end
 
 private
@@ -107,6 +106,5 @@ private
     rescue => exception
       raise "#{attr_name} debe ser integer. Valor encontrado: #{attr_value}"
     end
-    #raise "#{attr_name} debe ser integer. Valor encontrado: #{attr_value}" if !(attr_value.is_a? Integer)
   end
 end
--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
 
-attr_name = "PRUEBA"
-attr_value = "A"
-raise "#{attr_name} debe ser integer. Valor encontrado: #{attr_value}" if !(attr_value.is_a? Integer)
+attr_name = 'PRUEBA'
+attr_value = 'A'
+raise "#{attr_name} debe ser integer. Valor encontrado: #{attr_value}" unless (attr_value.is_a? Integer)
